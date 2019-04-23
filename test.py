02f66ea9b156d03a2f241f98e9e1be0b1167dbf7
Ravi <selker.ravi@gmail.com>
2019-04-23 11:47:34 +0200

    Improvements

--- test.py
+++ test.py
@@ -6,40 +6,36 @@ class TestCore(unittest.TestCase):
 
     def test_calc_high_freq(self):
 
-        word_string = 'The sun shines over the lake'
+        test_1 = WordFrequencyAnalyzer('The sun shines over the lake')
+        self.assertEqual(test_1.calc_high_freq(), 2)
 
-        test_1 = WordFrequencyAnalyzer.calc_high_freq(word_string)
-        self.assertEqual(test_1, 2)
+        test_2 = WordFrequencyAnalyzer('')
+        self.assertEqual(test_2.calc_high_freq(), 0)
 
-        test_2 = WordFrequencyAnalyzer.calc_high_freq('')
-        self.assertEqual(test_2, 0)
-
-        test_3 = WordFrequencyAnalyzer.calc_high_freq('NOT not NoT nOt')
-        self.assertEqual(test_3, 4)
+        test_3 = WordFrequencyAnalyzer('NOT not NoT nOt')
+        self.assertEqual(test_3.calc_high_freq(), 4)
 
     def test_calc_freq_word(self):
 
         word_string = 'It was the best of times it was the worst of times ' \
                       'it was the age of wisdom it was the age of foolishness'
 
-        test_1 = WordFrequencyAnalyzer.calc_freq_word(word_string, 'times')
-        self.assertEqual(test_1, 2)
+        test_1 = WordFrequencyAnalyzer(word_string)
+        self.assertEqual(test_1.calc_freq_word('times'), 2)
 
-        test_2 = WordFrequencyAnalyzer.calc_freq_word('it is time for a time-out', 'time')
-        self.assertEqual(test_2, 2)
+        test_2 = WordFrequencyAnalyzer('it is time for a time-out')
+        self.assertEqual(test_2.calc_freq_word('time'), 2)
 
-        test_3 = WordFrequencyAnalyzer.calc_freq_word('', 'time')
-        self.assertEqual(test_3, 0)
+        test_3 = WordFrequencyAnalyzer('')
+        self.assertEqual(test_3.calc_freq_word('times'), 0)
 
     def test_most_freq_word(self):
 
-        word_string = 'The sun shines over the lake'
+        test_1 = WordFrequencyAnalyzer('The sun shines over the lake')
+        self.assertEqual(test_1.calc_most_freq_word(3), [('the', 2), ('lake', 1), ('over', 1)])
 
-        test_1 = WordFrequencyAnalyzer.calc_most_freq_word(word_string, 3)
-        self.assertEqual(test_1, [('the', 2), ('lake', 1), ('over', 1)])
-
-        test_2 = WordFrequencyAnalyzer.calc_most_freq_word('', 2)
-        self.assertEqual(test_2, [])
+        test_2 = WordFrequencyAnalyzer('')
+        self.assertEqual(test_2.calc_most_freq_word(3), [])
 
 
 if __name__ == '__main__':
--- wordfreq.py
+++ wordfreq.py
@@ -1,62 +1,50 @@
 import re
+from collections import Counter
 
 
 class WordFrequency:
 
-    def word_freq(self):
-        """Return word frequency of all words in a string"""
-        if not isinstance(self, str):
-            raise TypeError('\'self\' should be of type string')
+    def word(self):
+        pass
 
-        # split string into words
-        word_list = re.split('[^a-zA-Z]', self.lower())
-        word_list = list(filter(None, word_list))
-
-        # add word frequencies and make dictionary
-        word_freq = [(p, word_list.count(p)) for p in word_list]
-        freq_dict = dict(word_freq)
-
-        return freq_dict
+    def frequency(self):
+        pass
 
 
 class WordFrequencyAnalyzer:
 
+    __word_freq_sorted = None
+
+    def __init__(self, text):
+        if not isinstance(text, str):
+            raise TypeError('input should be of type string')
+
+        # split string into words
+        word_list = re.split('[^a-zA-Z]', text.lower())
+        word_list = list(filter(None, word_list))
+
+        # get word freq and sort by frequency + alphabetical
+        word_freq = Counter(word_list).most_common()
+        self.__word_freq_sorted = sorted(word_freq, key=lambda l: (-l[1], l[0]))
+
     def calc_high_freq(self):
         """Return the highest word frequency of a string."""
-        if not isinstance(self, str):
-            raise TypeError('\'self\' should be of type string')
-
-        freq_dict = WordFrequency.word_freq(self)
-
-        # return the largest frequency
-        if freq_dict:
-            return freq_dict[max(freq_dict, key=freq_dict.get)]
+        if self.__word_freq_sorted:
+            return self.__word_freq_sorted[0][1]
         else:
             return 0
 
     def calc_freq_word(self, word):
         """Return the word frequency of a word in a string."""
-        if not isinstance(self, str):
-            raise TypeError('\'self\' should be of type string')
-
-        freq_dict = WordFrequency.word_freq(self)
-
-        # return frequency of word
-        if word in freq_dict:
-            return freq_dict[word]
+        if word in dict(self.__word_freq_sorted):
+            return dict(self.__word_freq_sorted)[word]
         else:
             return 0
 
     def calc_most_freq_word(self, n):
         """Return the most frequent n words in a string."""
-        if not isinstance(self, str):
-            raise TypeError('\'self\' should be of type string')
+        return self.__word_freq_sorted[:n]
 
-        freq_dict = WordFrequency.word_freq(self)
-
-        # make list and sort
-        freq_dict_sorted = [(key, freq_dict[key]) for key in freq_dict]
-        freq_dict_sorted.sort(key=lambda l: (-l[1], l[0]))
-
-        # return 1:n highest frequency words
-        return freq_dict_sorted[:n]
+    def get_word_freq(self):
+        """Return a list of word frequencies."""
+        return self.__word_freq_sorted
