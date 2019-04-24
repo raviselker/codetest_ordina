657a3a701b931da95732cb39dae26f067b6ee1f6
Ravi <selker.ravi@gmail.com>
2019-04-24 22:49:25 +0200

    Improved code
    No more errors from flake8 + 100% code coverage

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import unittest
-from wordfreq import *
+from wordfreq import WordFrequencyAnalyzer, WordFrequency
 
 
 class TestCore(unittest.TestCase):
+
+    def test_word_freq_class(self):
+        self.assertEqual(WordFrequency('time', 4).__str__(),
+                         "'time' has a word frequency of 4")
+
+    def test_word_freq_analyzer_class(self):
+        with self.assertRaises(TypeError):
+            WordFrequencyAnalyzer(3)
 
     def test_calc_high_freq(self):
 
@@ -29,6 +37,9 @@ class TestCore(unittest.TestCase):
         test_3 = WordFrequencyAnalyzer('')
         self.assertEqual(test_3.calc_freq_word('times'), 0)
 
+        with self.assertRaises(TypeError):
+            test_1.calc_freq_word(3)
+
     def test_most_freq_word(self):
 
         test_1 = WordFrequencyAnalyzer('The sun shines over the lake')
@@ -39,6 +50,13 @@ class TestCore(unittest.TestCase):
 
         test_2 = WordFrequencyAnalyzer('')
         self.assertEqual(test_2.calc_most_freq_words(3), [])
+
+    def test_get_word_freq(self):
+
+        test_1 = WordFrequencyAnalyzer('NOT not NoT nOt')
+        word_freq = test_1.get_word_freq()
+
+        self.assertEqual(word_freq, [('not', 4)])
 
 
 if __name__ == '__main__':
--- wordfreq.py
+++ wordfreq.py
@@ -15,7 +15,11 @@ class WordFrequency:
         return self.word == other.word and self.freq == other.freq
 
     def __lt__(self, other):
-        return self.word > other.word if self.freq == other.freq else self.freq < other.freq
+        return (
+            self.word > other.word
+            if self.freq == other.freq
+            else self.freq < other.freq
+        )
 
 
 class WordFrequencyAnalyzer:
@@ -23,6 +27,7 @@ class WordFrequencyAnalyzer:
     __word_freq = []
 
     def __init__(self, text: str):
+        # check whether text is a string
         if not isinstance(text, str):
             raise TypeError('input should be of type string')
 
@@ -36,7 +41,7 @@ class WordFrequencyAnalyzer:
     def calc_high_freq(self) -> int:
         """
         Return the highest word frequency of a string.
-        :returns: highest frequency
+        :returns: integer: highest frequency
         """
         if self.__word_freq:
             return self.__word_freq[0][1]
@@ -46,9 +51,11 @@ class WordFrequencyAnalyzer:
     def calc_freq_word(self, word: str) -> int:
         """
         Return the word frequency of a word in a string.
-        :param word: The word for which the frequency needs to be returned
-        :returns: frequency of 'word'
+        :param String word: The word for which the frequency needs to be
+        returned
+        :returns: integer: frequency of 'word'
         """
+        # check whether word is a string
         if not isinstance(word, str):
             raise TypeError('\'word\' should be of type string')
 
@@ -60,10 +67,12 @@ class WordFrequencyAnalyzer:
     def calc_most_freq_words(self, n: int):
         """
         Return the most frequent n words in a string.
-        :param n: The number of word frequencies that need to be returned
+        :param integer n: The number of word frequencies that need to be
+        returned
         :returns: a list of WordFrequency objects
         """
-        word_freq_list = [WordFrequency(word, freq) for word, freq in self.__word_freq]
+        word_freq_list = [WordFrequency(word, freq)
+                          for word, freq in self.__word_freq]
         return sorted(word_freq_list, reverse=True)[:n]
 
     def get_word_freq(self):
